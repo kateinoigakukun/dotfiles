bbf488f23b45e9e003f4c73b9cb174599045f73d
Saito Yuta <yuutas4@SYuta-MacBook-Pro.local>
2016-05-20 14:02:40 +0900

    fix comment

--- install.sh
+++ install.sh
@@ -2,31 +2,31 @@ if [ ! `which brew` ]; then
   echo "Installing Homebrew ..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-echo "Installing Vim"
-brew install vim
+echo "Installing Vim ..."
+sudo brew install vim
 
 if [ ! `which tmux` ]; then
-  echo "Installing Tmux"
-  brew install tmux
+  echo "Installing Tmux ..."
+  sudo  brew install tmux
 fi
 if [ ! -e ~/.tmux/plugins/tpm ]; then
-  echo "Installing Tmux Plugin Manager..."
+  echo "Cloning Tmux Plugin Manager..."
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
-echo "Downloading solarized"
+echo "Downloading solarized ..."
 git clone https://github.com/tomislav/osx-terminal.app-colors-solarized ~/Desktop/solarized
-echo "Make symbolic link"
+echo "Make symbolic link ..."
 ln -s ~/dotfiles/_vimrc ~/.vimrc
 ln -s ~/dotfiles/_zshrc ~/.zshrc
 ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/_gvimrc ~/.gvimrc
-echo "Copy pached font"
+echo "Copy pached font ..."
 cp ~/dotfiles/myDroidSansMonoForPowerline.ttf ~/Library/Fonts
-echo "Change default shell to zsh"
-chsh -s /usr/local/bin/zsh
-echo "Installing Prezto"
+echo "Change default shell to zsh ..."
+sudo chsh -s /usr/local/bin/zsh
+echo "Cloning Prezto ..."
 git clone  https://github.com/kateinoigakukun/prezto.git ~/.zprezto
-##自分のリポに追加してそこから読み込む
+
 echo "Done!"
 echo "--------------------------------------------------"
 echo "Please change Terminal font to myDroidSansMonoForPowerline."
