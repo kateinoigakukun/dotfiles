e8bdcce6505f2c331e1fd89b76e99c58095de4af
Yuta Saito <kateinoigakukun@gmail.com>
2019-08-03 09:50:44 +0900

    Install goenv

--- .zshenv
+++ .zshenv
@@ -6,6 +6,7 @@ export SAVEHIST=100000
 export XDG_CONFIG_HOME=$HOME/.config
 export PGDATA=/usr/local/var/postgres
 export PYENV_ROOT=$HOME/.pyenv
+export GOENV_ROOT=$HOME/.goenv
 export GOPATH=$HOME/.go
 export DOTFILE_PATH=$HOME/dotfiles
 export ZPLUG_HOME=$HOME/.zplug
@@ -15,6 +16,7 @@ path=(
     $HOME/.local/bin(N-/)
     $HOME/.cabal/bin(N-/)
     $PYENV_ROOT/bin(N-/)
+    $GOENV_ROOT/bin(N-/)
     $path
 )
 
--- zsh/functions/xenv.zsh
+++ zsh/functions/xenv.zsh
@@ -1,4 +1,5 @@
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+if which rbenv    > /dev/null; then eval "$(rbenv init -)";    fi
+if which pyenv    > /dev/null; then eval "$(pyenv init -)";    fi
 if which swiftenv > /dev/null; then eval "$(swiftenv init -)"; fi
-if which nodeenv > /dev/null; then eval "$(nodenv init -)"; fi
+if which nodeenv  > /dev/null; then eval "$(nodenv init -)";   fi
+if which goenv    > /dev/null; then eval "$(goenv init -)";    fi
